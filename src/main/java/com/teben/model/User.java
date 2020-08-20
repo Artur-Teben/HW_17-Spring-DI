916b3d111821c7dc5c9c2f4818867e36b56dee62
Artur Teben <teben.artur99@gmail.com>
2020-08-20 14:17:21 +0300

    Create class User

---
+++ src/main/java/com/teben/model/User.java
@@ -0,0 +1,49 @@
+package com.teben.model;
+
+public class User {
+    private String name;
+    private Integer age;
+    private String city;
+
+    public User() {
+    }
+
+    public User(String name, Integer age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
